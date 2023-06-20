aa6a05340c4dbbc248c3f699cf3e6fc4c7290cff
haroldhang <haroldadjahi@gmail.com>
2023-06-20 12:30:17 +0100

    deploy the app

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,38 @@
-#!/usr/bin/env bash
-echo 'Running composer'
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+#!/bin/bash
 
-echo 'Caching config...'
-php artisan config:cache
+cd /usr/share/nginx/html/
 
-echo 'Caching routes...'
-php artisan route:cache
 
-echo 'Running migrations...'
-php artisan migrate --force
+if [ "$(sed -n '3p' .env)" = "$(sed -n '3p' .env.example)" ]; then
+    php artisan key:generate
+    php artisan migrate
+    php artisan db:seed
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
+    php artisan clear-compiled
+    
+else
+    php artisan migrate
+    php artisan db:seed
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
+    php artisan clear-compiled
+
+fi
+
+#
+
+# Update nginx to match worker_processes to no. of cpu's
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
+
+
+# Always chown webroot for better mounting
+chown -Rf nginx:nginx /usr/share/nginx/html
+
+# Start supervisord and services
+/usr/local/bin/supervisord -n -c /etc/supervisord.conf
