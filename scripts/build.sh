815f634250b2fd4a51680852247e5d5213385ec4
haroldhang <haroldadjahi@gmail.com>
2023-09-06 17:35:28 +0100

    some changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 php artisan key:generate
-php artisan migrate
-php artisan db:seed
+#php artisan migrate
+#php artisan db:seed
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
